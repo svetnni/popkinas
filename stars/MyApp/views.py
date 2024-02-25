a1b4c52c9bf3740c177fd6121223dd525d1e6996
Pupil <nedaskovskaasvetlana23@gmail.com>
2024-02-25 18:32:05 +0300

    sd

--- stars/MyApp/views.py
+++ stars/MyApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def helloworld(request):
+    return render(request, 'helloworld.html')
+
+
+def star(request):
+    return render(request, 'star2.html')
